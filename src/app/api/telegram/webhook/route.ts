3c6e4d504edda06d288bdaa48b12ec4fb860eab3
tttora <ttaktorgyn2006@gmail.com>
2026-08-04 02:57:23 +0500

    webhook log

--- src/app/api/telegram/webhook/route.ts
+++ src/app/api/telegram/webhook/route.ts
@@ -3,7 +3,14 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const update = await req.json();
 
-  console.log(update);
+  console.log("NEW UPDATE:");
+  console.log(JSON.stringify(update, null, 2));
 
   return NextResponse.json({ ok: true });
+}
+
+export async function GET() {
+  return NextResponse.json({
+    status: "Webhook is working ✅",
+  });
 }
