ecf0ace2600ff2e5dc77162fddd7e1f99f993881
tttora <ttaktorgyn2006@gmail.com>
2026-08-05 03:14:13 +0500

    debug2

--- src/components/OrderModal.tsx
+++ src/components/OrderModal.tsx
@@ -74,7 +74,7 @@ export default function OrderModal({
 
                 ⏳ Төлем күтілуде
             `,
-            orederCode: newOrderCode,
+            orderCode: newOrderCode,
         }),
     });
 
